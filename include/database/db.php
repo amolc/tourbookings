c8542d0ed526621dcb86636cc8d4ecf80ea0e45e
saurabhundre <saurabh@fountaintechies.com>
2014-10-26 19:39:50 -0700

    db
    db

--- include/database/db.php
+++ include/database/db.php
@@ -1,6 +1,12 @@
 <?php 
 //connect to db
-	$con = mysql_connect('localhost','root', '10gXWOqeaf');
+	if($_SERVER['HTTP_HOST']=="localhost"){
+	$con = mysql_connect('localhost','root', '');
 	$db	 = mysql_select_db('tourbookings-beta');
 	// $db	 = mysql_select_db('make_my_tour');
+}
+else{
+$con = mysql_connect('localhost','root', '10gXWOqeaf');
+	$db	 = mysql_select_db('tourbookings-beta');
+}
 ?>
