28ba09809b5e6fe84ee23bdb22501ceb6caa5512
gokul <gokulgapat@gmail.com>
2014-11-04 16:10:25 +0530

    db.php modified

--- include/database/db.php
+++ include/database/db.php
@@ -1,12 +1,15 @@
 <?php 
 //connect to db
-	if($_SERVER['HTTP_HOST']=="localhost"){
+if($_SERVER['HTTP_HOST']=="localhost"){
 	$con = mysql_connect('localhost','root', '');
-	$db	 = mysql_select_db('tourbookings-beta');
+	$db	 = mysql_select_db('tourbookings-beta');  
 	// $db	 = mysql_select_db('make_my_tour');
+}else if ($_SERVER['HTTP_HOST']=="localhost:8080") {
+	$con = @mysql_connect('localhost','root', '');
+    $db	 = @mysql_select_db('tourbookings-beta');
 }
 else{
-$con = mysql_connect('localhost','root', '10gXWOqeaf');
-	$db	 = mysql_select_db('tourbookings-beta');
+    //$con = mysql_connect('localhost','root', '10gXWOqeaf');
+	//$db	 = mysql_select_db('tourbookings-beta');
 }
 ?>
